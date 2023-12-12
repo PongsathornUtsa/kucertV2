131b384143f9c0b2a761dde05a50d98bff5b0f2a
PongsathornUtsa <peachutsa@gmail.com>
2023-12-12 11:26:07 +0700

    home

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,21 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { useAccount } from 'wagmi';
 import Drawers from "./components/drawers/Drawer";
 import Home from './pages/home/Home';
 import Dashboard from './pages/dashboard/Dashboard';
 
 function App() {
+  const { isConnected } = useAccount();
+
   return (
-      <BrowserRouter>
-        <Drawers>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/dashboard" element={<Dashboard />} />
-          </Routes>
-        </Drawers >
-      </BrowserRouter>
+    <BrowserRouter>
+      <Drawers>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/dashboard" element={isConnected ? <Dashboard /> : <Navigate to="/" replace />} />
+        </Routes>
+      </Drawers >
+    </BrowserRouter>
   );
 }
 
--- frontend/src/pages/home/Home.tsx
+++ frontend/src/pages/home/Home.tsx
@@ -2,29 +2,41 @@ import { Box, Grid, Typography } from '@mui/material';
 
 const Home = () => {
   return (
-    <Box 
-      sx={{ 
-        width: '100%', 
-        height: '100vh', // This sets the height to the full viewport height
-        display: 'flex', // Enables flexbox
-        flexDirection: 'column', // Stacks children vertically
-        alignItems: 'center', // Centers children horizontally
-        justifyContent: 'center', // Centers children vertically
-        p: 3 // Adds padding
-      }}
-    >
-      {/* Heading */}
-      <Typography variant="h2" sx={{ fontWeight: 'bold', textAlign: 'center', mb: 4 }}>
-        Secure Your Academic Achievements Forever
-      </Typography>
-      
-      {/* Subtitle and Description */}
-      <Typography variant="h5" sx={{ textAlign: 'center', mb: 3 }}>
-        Welcome to the future of academic credential verification.
-      </Typography>
-      <Typography sx={{ textAlign: 'center' }}>
-        Our platform leverages the unbreakable security of blockchain technology to mint your academic certificates as Non-Fungible Tokens (NFTs).
-      </Typography>
+    <Box sx={{ flexGrow: 1, p: 3, mt: 3 }}>
+      <Grid container spacing={2} alignItems="stretch" justifyContent="center">
+        {/* Text Section */}
+        <Grid item xs={12} md={6}>
+          <Box sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', height: '100%' }}>
+            <Typography variant="h2" sx={{ fontWeight: 'bold' }}>
+              Secure Academic Achievements 
+            </Typography>
+            <Typography variant="h2" sx={{ fontWeight: 'bold',color:"#045e57", mb: 4 }}>
+              Forever!!
+            </Typography>
+            <Typography variant="h5" sx={{ mb: 3 }}>
+              Welcome to the future of academic credential verification.
+            </Typography>
+            <Typography sx={{ mb: 6 }}>
+              Our platform leverages the unbreakable security of blockchain technology to mint your academic certificates as Non-Fungible Tokens (NFTs).
+            </Typography>
+            <Typography variant="h5" sx={{fontWeight: 'bold', mb: 6 }}>
+              Pongsathorn Utsahawattanasuk 6210554784
+            </Typography>
+          </Box>
+        </Grid>
+
+        {/* Image Section */}
+        <Grid item xs={12} md={6}>
+          <Box sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'flex-end', height: '100%' }}>
+            <Box
+              component="img"
+              sx={{width: '100%', alignSelf: 'flex-end' }}
+              alt="Descriptive alt text"
+              src="/vector.svg" 
+            />
+          </Box>
+        </Grid>
+      </Grid>
     </Box>
   );
 }
